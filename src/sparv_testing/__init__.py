acf99695646d31939e6052c2882681587c7b3d17
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-05-29 16:21:26 +0200

    refactor: rename package

--- src/sparv_testing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Testing utilities for sparv-pipeline."""
-
-from sparv_testing.annotations import MockAnnotation
-from sparv_testing.outputs import MemoryOutput
-
-__all__ = ["MemoryOutput", "MockAnnotation"]
